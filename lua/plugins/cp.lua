36fde35a0d62b50b02c03fa33f7d626e7798c7eb
anish384 <desaianish351@outlook.com>
2025-09-07 15:23:06 +0530

    improved

--- lua/plugins/cp.lua
+++ lua/plugins/cp.lua
@@ -1,5 +1,30 @@
 return{
-	'xeluxee/competitest.nvim',
-	dependencies = 'MunifTanjim/nui.nvim',
-	config = function() require('competitest').setup() end,
+    'xeluxee/competitest.nvim',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function() 
+        require('competitest').setup({
+            compile_command = {
+                c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+                cpp = { exec = "g++", args = { "-I", os.getenv("HOME").. "/.local/include","-std=c++20","-DlocalInclude","-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+                rust = { exec = "rustc", args = { "$(FNAME)" } },
+                java = { exec = "javac", args = { "$(FNAME)" } },
+            },
+            popup_ui = {
+                total_width = 0.8,
+                total_height = 0.8,
+                layout = {
+                    { 2, "tc" },
+                    { 3, {
+                        { 1, "so" },
+                        { 1, "si" },
+                    } },
+                    { 3, {
+                        { 1, "eo" },
+                        { 1, "se" },
+                    } },
+                }
+            },
+        }) 
+    end,
 }
+
