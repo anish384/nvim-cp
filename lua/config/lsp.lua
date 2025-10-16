483f8f8ab1e66ad8e07f73655d6155b9358b50c2
anish384 <124502658+anish384@users.noreply.github.com>
2025-10-16 20:44:01 +0530

    Refactor clangd configuration for Neovim LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,18 +1,14 @@
 -- ~/.config/nvim/lua/config/lsp.lua
 -- Neovim lsp (clangd) configuration
 
-local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.lsp.config.clangd = {
+  cmd = { 
+      'clangd', 
+      '--background-index',
+      '--header-insertion=never',
+  },
+  -- root_markers = { 'compile_commands.json', 'compile_flags.txt' },
+  filetypes = { 'c', 'cpp' },
+}
 
-lspconfig.clangd.setup({
-  capabilities = capabilities,
-  cmd = {
-    "clangd",
-    "--header-insertion=never", -- automatic header insertion is disabled
-    "--background-index",  -- Better performance
-    "--clang-tidy",       -- Optional: Enable clang-tidy
-    "--offset-encoding=utf-16",  -- Required for Neovim 0.10+
-  },  -- ensure clangd is installed and in PATH
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-})
-
+vim.lsp.enable({'clangd'})
